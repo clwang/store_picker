cfe45bfab84d6e8b14c089450e0c50b49e2d2d2f
Charles Wang <charles@thredup.com>
2017-01-12 20:45:14 -0800

    Displaying stores as the user types

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -4,9 +4,27 @@ var Main = React.createClass({
   },
 
   componentDidMount() { 
-    $.getJSON('https://location-winnow.herokuapp.com/locations', (response) => { 
-      this.setState({ stores: response }) 
-    }); 
+    // $.getJSON('https://location-winnow.herokuapp.com/locations', (response) => { 
+    //   this.setState({ stores: response }) 
+    // }); 
+  },
+
+  fetchResults(term) {
+    if (term == "" || term == undefined) {
+      // reset the stores
+      this.setState(this.getInitialState)
+    } else {
+      $.ajax({
+        url: "https://location-winnow.herokuapp.com/locations?query=" + encodeURI(term),
+        type: "GET",
+        dataType: "JSON",
+        success: (response) => {
+          console.log(response);
+          this.setState({ stores: response });
+        }
+      })
+    }
+    
   },
 
   render() {
@@ -14,8 +32,8 @@ var Main = React.createClass({
       <div>
         <h1>Welcome, Charles!</h1>
         <h2>Pick Your Stores So We Can Help You Hire.</h2>
-        <Search />
-        <SearchResults stores={this.state.stores} />
+        <Search fetchResults={this.fetchResults} />
+        <SearchResults stores={this.state.stores}  />
       </div>
     )
   }
--- app/assets/javascripts/components/_search.js.jsx
+++ app/assets/javascripts/components/_search.js.jsx
@@ -1,10 +1,14 @@
 var Search = React.createClass({
+    fetchResults() {
+        this.props.fetchResults(this.refs.search.value);
+    },
+
     render() {
         return (
             <div>
                 <label htmlFor="search">Search for stores:</label>
                 <br />
-                <input type="text" placeholder='Enter your address' />
+                <input ref='search' type="text" placeholder='Enter your address' onChange={this.fetchResults} />
             </div>
         )
     }
