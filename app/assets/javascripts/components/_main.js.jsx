f0e15c2dd61d19467d3b89d3d7c59f072e9bef6c
Charles Wang <charles@thredup.com>
2017-01-12 19:59:26 -0800

    Adding the search bar component

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,11 @@
+var Main = React.createClass({
+  render() {
+    return(
+      <div>
+        <h1>Welcome, Charles!</h1>
+        <h2>Pick Your Stores So We Can Help You Hire.</h2>
+        <Search />
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_search.js.jsx
@@ -0,0 +1,11 @@
+var Search = React.createClass({
+    render() {
+        return (
+            <div>
+                <label for="search">Search for stores:</label>
+                <br />
+                <input type="text" placeholder='Enter your address' />
+            </div>
+        )
+    }
+});
